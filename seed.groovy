1a9878249e769fa93257a36fbb0e45649f919959
tirumalaraju <sireesha@zelarsoft.com>
2021-05-18 17:05:33 +0530

    new

--- seed.groovy
+++ seed.groovy
@@ -10,25 +10,16 @@ for (i in 0..count) {
   def j=component[i]
   pipelineJob("CI-Pipelines/${j}-ci") {
     configure { flowdefinition ->
-      flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-        'triggers' {
-          'hudson.triggers.SCMTrigger' {
-            'spec'('*/2 * * * 1-5')
-            'ignorePostCommitHooks'(false)
-          }
-        }
-      }
       flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
         'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
           'userRemoteConfigs' {
             'hudson.plugins.git.UserRemoteConfig' {
               'url'('https://github.com/sireeshazs/'+j+'.git')
-              'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
             }
           }
           'branches' {
             'hudson.plugins.git.BranchSpec' {
-              'name'('*/tags/*')
+              'name'('*/main')
             }
           }
         }
@@ -38,3 +29,24 @@ for (i in 0..count) {
     }
   }
 }
+
+pipelineJob("CI-Pipelines/frontend-ci") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'('https://github.com/sireeshazs/frontend.git')
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
+          }
+        }
+        'scriptPath'('Jenkinsfile')
+        'lightweight'(true)
+      }
+    }
+  }
