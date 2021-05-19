9c1a3faa838825f088e7c7e2e13bbcfb49200882
swathi-zelarsoft680 <82649007+swathi-zelarsoft680@users.noreply.github.com>
2021-05-19 16:10:46 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -29,24 +29,3 @@ for (i in 0..count) {
     }
   }
 }
-
-pipelineJob("CI-Pipelines/frontend-ci") {
-    configure { flowdefinition ->
-      flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-        'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-          'userRemoteConfigs' {
-            'hudson.plugins.git.UserRemoteConfig' {
-              'url'('https://github.com/sireeshazs/frontend.git')
-            }
-          }
-          'branches' {
-            'hudson.plugins.git.BranchSpec' {
-              'name'('*/main')
-            }
-          }
-        }
-        'scriptPath'('Jenkinsfile')
-        'lightweight'(true)
-      }
-    }
-  }
